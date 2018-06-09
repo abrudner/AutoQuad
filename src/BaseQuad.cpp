4b05d5d8f5e8db11577356532ded4722bc34faba
Alex Brudner <abrudnerz@gmail.com>
2018-06-09 13:36:31 -0400

    Initial commit and beginning of project

---
+++ src/BaseQuad.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   BaseQuad.cpp
+ * Author: alex
+ * 
+ * Created on June 9, 2018, 1:26 PM
+ */
+
+#include "BaseQuad.h"
+
+BaseQuad::BaseQuad()
+{
+}
+
+BaseQuad::BaseQuad(const BaseQuad& orig)
+{
+}
+
+BaseQuad::~BaseQuad()
+{
+}
+
---
+++ src/BaseQuad.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   BaseQuad.h
+ * Author: alex
+ *
+ * Created on June 9, 2018, 1:26 PM
+ */
+
+#ifndef BASEQUAD_H
+#define BASEQUAD_H
+
+class BaseQuad {
+public:
+	BaseQuad();
+	BaseQuad(const BaseQuad& orig);
+	virtual ~BaseQuad();
+private:
+
+};
+
+#endif /* BASEQUAD_H */
+
